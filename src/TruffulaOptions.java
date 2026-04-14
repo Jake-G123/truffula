1cef5d17680d77e818e83c3b06767eb39d28adf3
Jake-G123 <jakelgerken@gmail.com>
2026-04-13 17:19:48 -0700

    Wave 2 Completed

--- src/TruffulaOptions.java
+++ src/TruffulaOptions.java
@@ -102,9 +102,17 @@ public class TruffulaOptions  {
    */
   public TruffulaOptions(String[] args) throws IllegalArgumentException, FileNotFoundException {
     // TODO: Replace the below lines with your implementation
-    root = null;
-    showHidden = false;
-    useColor = false;
+    boolean showHidden = false;
+    boolean useColor = true;
+    // COVERT TO STANDARD FOR LOOP
+    for(String arg : args) {
+      if(arg.equals("-h")) showHidden = true;
+      else if(arg.equals("-nc")) useColor = false;
+      else if (!args[args.length-1].equals(arg) && !arg.equals("-h") && !arg.equals("-nc"))throw new IllegalArgumentException("Unsupported flag");
+    }
+    this.showHidden = showHidden;
+    this.useColor = useColor;
+    root = new File(args[args.length-1]);
   }
 
   /**
